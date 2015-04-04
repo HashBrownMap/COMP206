4e8c00e612215610efad4317d872fef002866869
Zixi CHEN <zchen66@teaching.CS.McGill.CA>
2015-04-04 16:15:13 -0400

    Non Internal Errors

--- login.c
+++ login.c
@@ -2,31 +2,72 @@
 #include<stdlib.h>
 #include<string.h>
 
-int main(void){
+
+int find(char *str){
 FILE *memberslog;
-int find=0;
-char temp[200];
-char *data=getenv("QUERY_STRING");
+char temp[300];
 
+memberslog=fopen("members.txt", "rt");
 
-memberslog = fopen("members.txt", "rt");
 if(memberslog == NULL){
-printf("<p> MEMBERS.TXT CANT BE FOUND</P>");
+	return 0;
 }
-printf("<p>%s</p>",data);
-
-
-while(fgets(temp,200, memberslog) != NULL){
-	if((strstr(temp, data)) != NULL){
-		printf("<p>WELCOME TO THIS PAGE</p>");
-		find =1;
+while(!feof(memberslog)){
+while(fscanf(memberslog,"%s",temp) == 1)
+{
+	if((strstr(str,temp)) != NULL){
+		return 1;
 	}
 }
-fclose(memberslog);
-
-if(find ==0){
-	printf("<p>YOU ARE NOT A MEMBER OF THIS PAGE</p>");
 }
+fclose(memberslog);
+return 0;
+}
+
+int main(void){
+//int find=0;
+char string[200];
+char *data;
+int n=atoi(getenv("CONTENT_LENGTH"));
+int a = 0;
+char c;
+
+while((c = getchar ()) != EOF && a<n){
+	if(a<200){
+		if(c!='+'){
+		string[a]=c;
+	}
+	else{
+	string[a]=' ';
+	}
+	a++;
+	}
+}
+string[a]='\0';
+
+printf("Content-Type:text/html\n\n");
+printf("<html>");
+//printf("<head>TEST</head>");
+//printf("<title>LOGIN RESULT</title>");
+//data = getenv("QUERY_STRING");
+
+//if(data == NULL){
+//printf("<p> data is non existant</p>");
+//}
+
+//printf("<p>%s</p>",data);
+
+
+	if(find(string) == 1){
+		printf("<p>WELCOME TO THIS PAGE</p>");
+	}
+	else{
+		printf("<p>FUCK NON-MEMBERS</P>");
+}
+
+
+printf("</html>");
+return 0;
 }
 
 
